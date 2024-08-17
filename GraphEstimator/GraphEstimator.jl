3247979f25299c69e5b35ea1e8a338170be4cf62
DeepWater3104 <redcurry1106@gmail.com>
2024-08-17 22:42:05 +0900

    update

--- GraphEstimator/GraphEstimator.jl
+++ GraphEstimator/GraphEstimator.jl
@@ -1,1 +1,28 @@
 using Printf
+using LinearAlgebra
+
+function EstimateX_from_SpikeSequence()
+end
+
+
+function StARS()
+end
+
+function Λ_temp()
+    StARS()
+end
+
+function α_temp()
+end
+
+function D_temp()
+end
+
+function minimize_F2norm()
+    Λ_temp()
+    α_temp()
+    D_temp()
+end
+
+
+
