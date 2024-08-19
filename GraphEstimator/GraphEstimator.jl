db05492e34be3d077914745e54c1742c1c9fe3d5
DeepWater3104 <redcurry1106@gmail.com>
2024-08-19 10:50:01 +0900

    update

--- GraphEstimator/GraphEstimator.jl
+++ GraphEstimator/GraphEstimator.jl
@@ -1,14 +1,14 @@
 using LinearAlgebra
 using Random
 
-function EnumerateSpikedNeuron( t1, t2, spike_time::Vector, spike_neuron::Vector, N )
+function EnumerateSpikedNeuron( t1, t2, SpikeTime::Vector, SpikeNeuron::Vector, N )
     i=1
     spike_binary = zeros( UInt32, N)
-    while spike_time[i] < t1
+    while SpikeTime[i] < t1
         i++
     end
-    while t1 <= spike_time[i] && spike_time[i] <= t2
-        spike_binary[spike_neuron[i]] == 1
+    while t1 <= SpikeTime[i] && SpikeTime[i] <= t2
+        spike_binary[SpikeNeuron[i]] == 1
         i++
     end
     return spike_binary
@@ -37,22 +37,21 @@ function EstimateX_from_SpikeSequence( spike_time::Vector, spike_neuron::Vector,
             end
         end
     end
-    X = numEvent_Yi1_Yj1 / (numEvent_Yi1_Yj1 + numEvent_Yi1_Yj0) - numEvent_Yi1_Yj0 / (numEvent_Yi1_Yj0 + numEvent_Yi0_Yj0)
+    # 要素ごとの割り算
+    X = numEvent_Yi1_Yj1 ./ (numEvent_Yi1_Yj1 .+ numEvent_Yi1_Yj0) .- numEvent_Yi1_Yj0 ./ (numEvent_Yi1_Yj0 .+ numEvent_Yi0_Yj0)
     return X
 end
 
 
-function StARS!()
+function Extract_SubPopulation( spike_time::Vector, spike_neuron::Vector, sub_population::Vector )
+
 end
 
-function calc_Λ( α, X, D )
-    Λ = (-1)* inv(( 1/α)*(X + D) + I(size(X,1)) )
-    for i=1:N
-        Λ[i, i] = 0.
-    end
-    StARS!( Λ)
-    return Λ
+
+function Extract_SubSequence( spike_time::Vector, spike_neuron::Vector, t1, t2 )
+    
 end
+
 
 function calc_α( X, Λ)
     temp = inv( I(size(X,1))-Λ) - I(size(X,1))
@@ -70,8 +69,8 @@ end
 function calc_D( α, Λ)
     D = α* ( inv(I(size(Λ,1))-Λ) - I(size(Λ,1)) )
     # take only diagonal elements
-    for i=1:size(Λ,1)
-        for i=1:size(Λ,1)
+    for i=1:size(D ,1)
+        for j=1:size(D ,1)
             if i != j
                 D[i,j] = 0.
             end
@@ -81,21 +80,71 @@ function calc_D( α, Λ)
 end
 
 
+function calc_Λ_fixedM( α, X, D , M )
+    Λ = (-1)* inv(( 1/α)*(X + D) + I(size(X,1)) )
+    for i=1:N
+        Λ[i, i] = 0.
+    end
+    #reduce element
+    
+    return Λ
+end
+
+function minimizeJ_fixedM( num_iteration )
+    Λ = calc_Λ_fixedM( α, X, D )
+    α = calc_α( X , Λ )
+    D  = calc_D ( α, Λ )
+    return Λ
+end
+
+function StARS!( SpikeTime::Vector, SpikeNeuron::Vector, T, num_iteration )
+    # derive subsequences
+    subseq = N*rand(2, K)
+    ψMkij = zeros(N,K,N,N)
+    for M=1:N
+        for k=1:K
+            Xsub = extract_subseq( SpikeTime, SpikeNeuron, subseq[1,k], subseq[2,k] )
+            minimizeJ_fixedM( num_iteration )
+            ψMkij [M, k, :, :] = 
+        end
+    end
+end
+
+function calc_Λ( α, X, D )
+    Λ = (-1)* inv(( 1/α)*(X + D) + I(size(X,1)) )
+    for i=1:N
+        Λ[i, i] = 0.
+    end
+    StARS!( Λ)
+    return Λ
+end
+
+function minimizeJ( X, num_iteration )
+    # initial state
+    α = rand()
+    D  = r*I(N)
+    Λ = zeros(N, N)
+
+    for i=1:num_iteration
+        Λ = calc_Λ( α, X, D )
+        α = calc_α( X , Λ )
+        D  = calc_D ( α, Λ )
+    end
+    return Λ
+end
+
+
+# hyper parameters
 Random.seed!(1)
 ϵ1 = 3.0 #msec
 ϵ2 = 3.0 #msec
 r = 1.0
+K = 100
+γ= 
 num_iteration = 5
-X = EstimateX_from_SpikeSequence( spike_time, spike_neuron, ϵ1, ϵ2, T, N )
-
-# initial state
-α = rand()
-D  = r*I(N)
-Λ = zeros(N, N)
+record_neuron = []
 
 # optimization
-for i=1:num_iteration
-    Λ = calc_Λ( α, X, D )
-    α = calc_α( X , Λ )
-    D  = calc_D ( α, Λ )
-end
+SpikeTime_SubPopulation, SpikeTime_SubPopulation = extract_subPopulation( SpikeTime, SpikeNeuron, record_neuron )
+X = EstimateX_from_SpikeSequence( SpikeTime, SpikeNeuron, ϵ1, ϵ2, T, N )
+Λ = minimizeJ( X, num_iteration )
